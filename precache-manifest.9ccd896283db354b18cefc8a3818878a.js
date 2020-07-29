570e320d26426ddcbf227a60eaa3fb1aeabe0e34
bomotayo <omotayobabatundee@gmail.com>
2020-07-28 20:48:47 -0400

    Updates

---
+++ precache-manifest.9ccd896283db354b18cefc8a3818878a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3625976dde5320bddf74883d2a0fa34",
+    "url": "/Library/index.html"
+  },
+  {
+    "revision": "827dfc1fcb304c001df9",
+    "url": "/Library/static/css/main.1c0f9a8e.chunk.css"
+  },
+  {
+    "revision": "10cc6af28d1359fca972",
+    "url": "/Library/static/js/2.0aa58845.chunk.js"
+  },
+  {
+    "revision": "be848911cdfb2e16a5e1c73b793bf480",
+    "url": "/Library/static/js/2.0aa58845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827dfc1fcb304c001df9",
+    "url": "/Library/static/js/main.1afaa2da.chunk.js"
+  },
+  {
+    "revision": "0e1aec799945bbc936d0",
+    "url": "/Library/static/js/runtime-main.e8a4ef3b.js"
+  }
+]);
